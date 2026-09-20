95ba81fe1145a3322cc916b067ae92f6458ebc12
Lina Krüger <schichten_feature_6x@icloud.com>
2026-09-20 12:22:18 +0200

    add server.js and routes.js

---
+++ routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// eine GET-Anfrage
+router.get('/', async(req, res) => {
+    res.send({ message: "Hello FIW!" });
+});
+
+module.exports = router;
